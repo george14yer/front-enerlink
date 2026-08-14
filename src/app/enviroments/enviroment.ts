801095ace4ebb5c13ced697f7080321cb7b82c41
george14yer <80540365+george14yer@users.noreply.github.com>
2026-08-14 14:04:17 -0500

    Update enviroment.ts

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,6 +1,8 @@
 export const environment = {
 
-    apiUrl: 'http://localhost:8000'
+    //apiUrl: 'http://localhost:8000'
+    production: true,
+    apiUrl: 'https://abc123.ngrok-free.app'
      //apiUrl: 'https://sixtieth-turtle-synopses.ngrok-free.dev'
 
 };
