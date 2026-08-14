d0ea56bdc602973ebbf0e6a719cfb8157d179a6b
george14yer <80540365+george14yer@users.noreply.github.com>
2026-08-14 14:50:47 -0500

    Update API URL for production environment

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -2,7 +2,7 @@ export const environment = {
 
     //apiUrl: 'http://localhost:8000'
     production: true,
-    apiUrl: 'https://abc123.ngrok-free.app'
+    apiUrl: 'https://sixtieth-turtle-synopses.ngrok-free.dev'
      //apiUrl: 'https://sixtieth-turtle-synopses.ngrok-free.dev'
 
 };
