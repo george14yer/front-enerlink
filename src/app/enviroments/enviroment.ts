a57d68f4d7c2e8b686e3768a5eaa50fd2903574f
george14yer <80540365+george14yer@users.noreply.github.com>
2026-08-17 16:17:50 -0500

    Update enviroment.ts

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -2,7 +2,8 @@ export const environment = {
 
     //apiUrl: 'http://localhost:8000'
     production: true,
-    apiUrl: 'https://sixtieth-turtle-synopses.ngrok-free.dev'
+    //apiUrl: 'https://sixtieth-turtle-synopses.ngrok-free.dev'
      //apiUrl: 'https://sixtieth-turtle-synopses.ngrok-free.dev'
+    apiUrl: 'https://enerlink.acoresolutions.com/'
 
 };
